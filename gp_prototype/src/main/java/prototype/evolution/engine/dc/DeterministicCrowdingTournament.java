dfd245ba40a45ebc2f1e826104f85d9ad1e9842a
pkoperek <pkoperek@gmail.com>
2013-03-27 10:53:02 +0100

    easier to read this way

--- gp_prototype/src/main/java/prototype/evolution/engine/dc/DeterministicCrowdingTournament.java
+++ gp_prototype/src/main/java/prototype/evolution/engine/dc/DeterministicCrowdingTournament.java
@@ -18,7 +18,7 @@ public class DeterministicCrowdingTournament implements Tournament {
     }
 
     private IGPProgram pickWithBestFitness(IGPProgram left, IGPProgram right) {
-        return left.getFitnessValue() > right.getFitnessValue() ? right : left;
+        return left.getFitnessValue() < right.getFitnessValue() ? left : right;
     }
 
     @Override
