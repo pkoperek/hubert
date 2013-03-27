58e2ccd7626ac249cef0fb057b86832076faa42e
pkoperek <pkoperek@gmail.com>
2013-03-27 10:41:31 +0100

    omitting some variables when building a solution

--- gp_prototype/src/main/java/prototype/evolution/genotype/GenotypeFactory.java
+++ gp_prototype/src/main/java/prototype/evolution/genotype/GenotypeFactory.java
@@ -1,5 +1,6 @@
 package prototype.evolution.genotype;
 
+import org.apache.commons.lang.StringUtils;
 import org.constretto.annotation.Configuration;
 import org.constretto.annotation.Configure;
 import org.jgap.InvalidConfigurationException;
@@ -7,21 +8,59 @@ import org.jgap.gp.impl.GPConfiguration;
 import org.jgap.gp.impl.GPGenotype;
 import prototype.data.container.DataContainer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GenotypeFactory {
 
     private int maxNodes = GPGenotypeBuilder.DEFAULT_MAX_NODES;
     private ChromosomeBuildingStrategyFactory.StrategyType solutionChromosomes;
+    private List<String> variablesToOmit = Collections.<String>emptyList();
 
     public GPGenotype createGPGenotype(GPConfiguration configuration, DataContainer dataContainer) throws InvalidConfigurationException {
         ChromosomeBuildingStrategyFactory buildingStrategyFactory = new ChromosomeBuildingStrategyFactory(solutionChromosomes);
-        ChromosomeBuildingStrategy buildingStrategy = buildingStrategyFactory.createStrategy(Arrays.asList(dataContainer.getVariableNames()));
+        List<String> variableNames = prepareVariableNames(Arrays.asList(dataContainer.getVariableNames()));
+        ChromosomeBuildingStrategy buildingStrategy = buildingStrategyFactory.createStrategy(variableNames);
 
         return GPGenotypeBuilder
                 .builder(buildingStrategy, configuration)
                 .setMaxNodes(maxNodes)
                 .build();
+    }
+
+    private List<String> prepareVariableNames(List<String> variables) {
+        ArrayList<String> filteredVariables = new ArrayList<>(variables);
+        filteredVariables.removeAll(variablesToOmit);
+        return filteredVariables;
+    }
+
+    public void setChromosomes(ChromosomeBuildingStrategyFactory.StrategyType chromosomes) {
+        this.solutionChromosomes = chromosomes;
+    }
+
+    private List<String> filterVariables(String[] variables) {
+        List<String> filteredVariables = new ArrayList<>();
+
+        for (int i = 0; i < variables.length; i++) {
+            if (StringUtils.isNotEmpty(variables[i])) {
+                filteredVariables.add(variables[i]);
+            }
+        }
+
+        return filteredVariables;
+    }
+
+    @Configure
+    public void setVariablesToOmit(
+            @Configuration(
+                    value = "genotype.omit.variables",
+                    defaultValue = ""
+            )
+            String variablesToOmit) {
+        String[] variables = variablesToOmit.split(",");
+        this.variablesToOmit = filterVariables(variables);
     }
 
     @Configure
@@ -42,9 +81,5 @@ public class GenotypeFactory {
             )
             String solutionChromosomes) {
         this.solutionChromosomes = ChromosomeBuildingStrategyFactory.StrategyType.valueOf(solutionChromosomes);
-    }
-
-    public void setChromosomes(ChromosomeBuildingStrategyFactory.StrategyType chromosomes) {
-        this.solutionChromosomes = chromosomes;
     }
 }
