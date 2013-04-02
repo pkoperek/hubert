88b8bfa620709a0cf5eafbe3545745702701a76d
pkoperek <pkoperek@gmail.com>
2013-04-02 14:47:24 +0200

    experiment script

--- gp_prototype/src/main/scripts/experiment.sh
+++ gp_prototype/src/main/scripts/experiment.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data.properties >& full_data.log &
-java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data_moving_window.properties >& moving_window.log &
+SLEEPTIME=1890
+
+if [ -z $1 ]; then
+        echo "Please provide number of repetitions as first argument"
+        exit
+fi
+
+for I in `seq $1`; do
+        echo "Attempt: $I";
+        java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data.properties >& full_data.log.$I &
+        java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data_moving_window.properties >& moving_window.log.$I &
+        echo "Sleeping: $SLEEPTIME"
+        sleep $SLEEPTIME
+done;
