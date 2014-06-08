9b63b2509c6ea5c49311dd337765a792cc4be9f4
Pawel Koperek <pkoperek@gmail.com>
2014-06-08 17:08:55 +0100

    running script

--- hubert/src/main/scripts/experiment.sh
+++ hubert/src/main/scripts/experiment.sh
@@ -9,8 +9,8 @@ fi
 
 for I in `seq $1`; do
         echo "Attempt: $I";
-        java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data.properties >& full_data.log.$I &
-        java -jar lib/gp_prototype-1.0-SNAPSHOT.jar config/remote_monitoring_data_moving_window.properties >& moving_window.log.$I &
+        java -jar lib/hubert-2.0-SNAPSHOT.jar config/remote_monitoring_data.properties >& full_data.log.$I &
+        java -jar lib/hubert-2.0-SNAPSHOT.jar config/remote_monitoring_data_moving_window.properties >& moving_window.log.$I &
         echo "Sleeping: $SLEEPTIME"
         sleep $SLEEPTIME
 done;
