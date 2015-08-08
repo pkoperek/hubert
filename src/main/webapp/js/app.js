1d0989dffa27edb4e97c6728a52bbac694cff03d
Pawel Koperek <pkoperek@gmail.com>
2015-08-08 18:20:18 +0100

    basic version of modal dialogs

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,1 +1,65 @@
 var hubertApp = angular.module('hubertApp', ['ui.bootstrap']);
+
+hubertApp.controller('NavbarButtonController', function($scope, $modal, $log) {
+
+    var openModal = function(modalToOpen, successCallback, failureCallback) {
+        var new_experiment = $modal.open({
+            animation: true,
+            size: 'lg',
+            controller: modalToOpen + 'Controller',
+            templateUrl: modalToOpen + 'Modal.html'
+        });
+
+        new_experiment.result.then(successCallback, failureCallback);
+    };
+
+    $scope.new_experiment = function() {
+        openModal(
+            'newExperiment',
+            function(result) {
+                $log.info("New experiment modal success: " + result);
+            },
+            function(reason) {
+                $log.info("New experiment modal cancelled: " + reason);
+            }
+        );
+        $log.info("defining new experiment");
+    };
+    
+    $scope.upload_experiment = function() {
+        openModal(
+            'uploadExperiment',
+            function(result) {
+                $log.info("New experiment modal success: " + result);
+            },
+            function(reason) {
+                $log.info("New experiment modal cancelled: " + reason);
+            }
+        );
+        $log.info("uploading experiment");
+    };
+});
+
+hubertApp.controller('newExperimentController', function($scope, $modalInstance) {
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+    
+    $scope.ok = function() {
+        $modalInstance.close('new experiment');
+    };
+
+});
+
+hubertApp.controller('uploadExperimentController', function($scope, $modalInstance) {
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.ok = function() {
+        $modalInstance.close('upload experiment');
+    };
+
+});
