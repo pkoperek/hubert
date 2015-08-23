353e0ffaf0e5efe5e0bf4e90007ab3ea23b8f87b
Pawel Koperek <pkoperek@gmail.com>
2015-08-23 18:08:10 +0100

    use config from file

--- src/main/scala/Scalatra.scala
+++ src/main/scala/Scalatra.scala
@@ -2,6 +2,7 @@ import javax.servlet.ServletContext
 
 import org.scalatra.LifeCycle
 import org.slf4j.LoggerFactory
+import pl.edu.agh.hubert.configuration.Configuration
 import pl.edu.agh.hubert.engine.EvolutionExecutor
 import pl.edu.agh.hubert.servlets._
 
@@ -12,7 +13,10 @@ class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
 
     logger.info("Initializing Scalatra")
-    val evolutionExecutor = new EvolutionExecutor(10, 5000)
+    val evolutionExecutor = new EvolutionExecutor(
+      Configuration.threads, 
+      Configuration.taskWaitTime
+    )
     
     logger.info("Servlets")
     context mount (new ExperimentsServlet, "/experiments")
