cb7c0db8184b7c6604e4e1b51b73f76abf7cfa89
Pawel Koperek <pkoperek@gmail.com>
2015-08-26 23:16:08 +0100

    show experiments on main page

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,21 +1,26 @@
 var hubertApp = angular.module('hubertApp', ['ui.bootstrap', 'ngTagsInput']);
 
+var openModal = function(modal, modalToOpen, successCallback, failureCallback, title, experimentModel) {
+    var modalInstance = modal.open({
+        animation: true,
+        size: 'lg',
+        controller: modalToOpen + 'Controller',
+        templateUrl: modalToOpen + 'Modal.html',
+        resolve: {
+            title: function() { return title || "!No title passed!"; },
+            experiment: function() { return experimentModel || {}; }
+        }
+    });
+
+    modalInstance.result.then(successCallback, failureCallback);
+};
+
 hubertApp.controller('NavbarButtonController', function($scope, $modal, $log, $http) {
-
-    var openModal = function(modalToOpen, successCallback, failureCallback) {
-        var new_experiment = $modal.open({
-            animation: true,
-            size: 'lg',
-            controller: modalToOpen + 'Controller',
-            templateUrl: modalToOpen + 'Modal.html'
-        });
-
-        new_experiment.result.then(successCallback, failureCallback);
-    };
 
     $scope.new_experiment = function() {
         openModal(
-            'newExperiment',
+            $modal,
+            'experiment',
             function(result) {
                 $log.info("New experiment to send: " + result);
                 $http.post("experiments/run", result).success(function(data) {
@@ -24,13 +29,15 @@ hubertApp.controller('NavbarButtonController', function($scope, $modal, $log, $h
             },
             function(reason) {
                 $log.info("New experiment modal cancelled: " + reason);
-            }
+            },
+            "Define new experiment"
         );
         $log.info("defining new experiment");
     };
     
     $scope.upload_experiment = function() {
         openModal(
+            $modal,
             'uploadExperiment',
             function(result) {
                 $log.info("Upload experiment modal success: " + result);
@@ -43,10 +50,29 @@ hubertApp.controller('NavbarButtonController', function($scope, $modal, $log, $h
     };
 });
 
-hubertApp.controller('newExperimentController', function($scope, $modalInstance, $log, configuration) {
-
-    $scope.configuration = configuration;
+hubertApp.controller('experimentsListController', function($scope, $modal, $log, $http) {
     
+    $scope.loadingIndicator = "(loading...)";
+    
+    $http.get("experiments").then(function(response) {
+        $log.info("Got list of experiments from service: " + response.data);
+        $scope.loadingIndicator = "";
+        $scope.experiments = response.data;
+    }, function(errorResponse) {
+        // TODO: Handle error case
+    });
+    
+});
+
+hubertApp.controller('experimentController', function($scope, $modalInstance, $log, configuration, title, experiment) {
+
+    $scope.title = title;
+    $scope.configuration = configuration;
+    $scope.experimentName = experiment.name;
+    $scope.description = experiment.description;
+    $scope.iterations = experiment.iterations;
+    $scope.selectedLanguage = experiment.language;
+
     $scope.loadWords = function() {
         $log.info("Loading words: " + $scope.selectedLanguage.words);
         
@@ -75,9 +101,9 @@ hubertApp.controller('newExperimentController', function($scope, $modalInstance,
     
         var newExperiment = {
             "id": -1,
-            "name": $scope.experimentName,
-            "description": $scope.experimentDescription,
-            "iterations": $scope.iterations,
+            "name": $scope.experimentName || "Experiment",
+            "description": $scope.experimentDescription || "Description",
+            "iterations": $scope.iterations || 10,
             "language": {
                 "name": $scope.selectedLanguage.name,
                 "words": classNames
