8aa5d81737119d319d77dfacfeaf11336e186609
Pawel Koperek <pkoperek@gmail.com>
2015-08-29 20:16:19 +0100

    fixing broken test

--- src/test/scala/pl/edu/agh/hubert/experiments/ExperimentTest.scala
+++ src/test/scala/pl/edu/agh/hubert/experiments/ExperimentTest.scala
@@ -8,18 +8,24 @@ import pl.edu.agh.hubert.experiments.ExperimentProtocol._
 
 class ExperimentTest extends FunSuite {
 
-  val experiment = new Experiment(1, "name of experiment", "description", Languages.mathLanguage())
-  
+  val experiment = new Experiment(
+    1,
+    "name of experiment",
+    "description",
+    10,
+    Languages.mathLanguage()
+  )
+
   test("serialize and deserialize experiment") {
     val experimentAsJson = experiment.toJson.toString()
     val deserializedExperiment = experimentAsJson.parseJson.convertTo[Experiment]
-    
+
     assert(experiment.name == deserializedExperiment.name)
     assert(experiment.description == deserializedExperiment.description)
-    
+
     // TODO: learn how to implement equals in scala and fix the test!
     assert(experiment.language == deserializedExperiment.language)
     assert(experiment.id == deserializedExperiment.id)
   }
-  
+
 }
