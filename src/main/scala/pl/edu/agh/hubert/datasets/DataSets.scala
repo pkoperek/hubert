a143a0b285fabd400a481ce862e2c4b18d5a5bf2
Pawel Koperek <pkoperek@gmail.com>
2015-09-01 23:42:55 +0100

    datasets repository implementation

--- src/main/scala/pl/edu/agh/hubert/datasets/DataSets.scala
+++ src/main/scala/pl/edu/agh/hubert/datasets/DataSets.scala
@@ -3,6 +3,7 @@ package pl.edu.agh.hubert.datasets
 import java.io.File
 
 import scala.collection.mutable.ArrayBuffer
+import scala.io.{BufferedSource, Source}
 import scalax.file.Path
 
 object DataSets {
@@ -13,17 +14,55 @@ object DataSets {
     storage += dataSet
   }
 
+  def clear(): Unit = {
+    storage.clear()
+  }
+
   def addFromPath(path: String): Unit = {
     val datasetDirectory: File = new File(path)
 
     if (!datasetDirectory.isDirectory) {
       throw new IllegalArgumentException("Path " + path + " is not a directory!")
     }
-    
+
     val csvFiles = datasetDirectory.listFiles.filter(x => x.getName.endsWith(".csv"))
-    
-    for(csvFile <- csvFiles) {
-      addDataSet(new DataSet(csvFile.getAbsolutePath, Set()))
+
+    for (csvFile <- csvFiles) {
+      val maybeDataSet = translateToDataSet(csvFile)
+      if (maybeDataSet.isDefined) {
+        addDataSet(maybeDataSet.get)
+      }
+    }
+  }
+
+  private def translateToDataSet(csvFile: File): Option[DataSet] = {
+    val line = firstLine(csvFile)
+
+    if (line.isDefined) {
+      if (line.get.startsWith("#")) {
+        val variables = line.get.replaceAll("#", "").replaceAll(" ", "").split(",")
+        return Some(new DataSet(csvFile.getAbsolutePath, variables.toSet))
+      } else {
+        return Some(new DataSet(csvFile.getAbsolutePath, Set()))
+      }
+    }
+
+    None
+  }
+
+  private def firstLine(csvFile: File): Option[String] = {
+    val source = Source.fromFile(csvFile)
+
+    try {
+      val lineIterator = source.getLines().take(1)
+      if (lineIterator.hasNext) {
+        Some(lineIterator.next())
+      } else {
+        None
+      }
+    }
+    finally {
+      source.close()
     }
   }
 
--- src/test/scala/pl/edu/agh/hubert/datasets/DataSetsTest.scala
+++ src/test/scala/pl/edu/agh/hubert/datasets/DataSetsTest.scala
@@ -3,31 +3,96 @@ package pl.edu.agh.hubert.datasets
 import java.io.File
 import java.nio.file.{Files, Path}
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
-class DataSetsTest extends FunSuite {
+import scala.collection.mutable
+import scalax.io.Resource
+
+class DataSetsTest extends FunSuite with BeforeAndAfter {
+
+  var directory: Path = null
+
+  before {
+    DataSets.clear()
+
+    directory = createTempDirectory
+  }
+
+  after {
+    deleteRecursively()
+  }
 
   test("should throw IllegalArgumentException if path not a directory") {
-    val file = temporaryFile
+    val file = File.createTempFile("temp", ".csv")
+    file.deleteOnExit()
 
     intercept[IllegalArgumentException] {
       DataSets.addFromPath(file.getAbsolutePath)
     }
   }
 
+  test("should create the data set if the file doesn't contain the variables line") {
+    val temporaryFileWithVariables: File = temporaryFile("test1.csv")
+    Resource.fromFile(temporaryFileWithVariables).writeStrings(Array(
+      "1,2,3"
+    ))
+
+    DataSets.addFromPath(directory.toFile.getAbsolutePath)
+
+    assert(DataSets.dataSets.nonEmpty)
+    val dataSet = DataSets.dataSets(0)
+
+    assert(dataSet.path == temporaryFileWithVariables.getAbsolutePath)
+    assert(dataSet.variables.isEmpty)
+  }
+
+  test("should ignore the file if it is empty") {
+    temporaryFile("test1.csv")
+
+    DataSets.addFromPath(directory.toFile.getAbsolutePath)
+
+    assert(DataSets.dataSets.isEmpty)
+  }
+
+  test("should read first line from csv and parse variable names") {
+    val temporaryFileWithVariables: File = temporaryFile("test1.csv")
+    writeCorrectContent(temporaryFileWithVariables)
+
+    DataSets.addFromPath(directory.toFile.getAbsolutePath)
+    val dataSet = DataSets.dataSets(0)
+
+    assert(dataSet.variables.contains("var1"))
+    assert(dataSet.variables.contains("var2"))
+    assert(dataSet.variables.contains("var3"))
+    assert(dataSet.variables.size == 3)
+  }
+
+  test("should ignore spaces in the first line") {
+    val temporaryFileWithVariables: File = temporaryFile("test1.csv")
+    writeContent(temporaryFileWithVariables, Array(
+      "#     var1    , var2   ,   var3   ",
+      "1,2,3"
+    ))
+
+    DataSets.addFromPath(directory.toFile.getAbsolutePath)
+    val dataSet = DataSets.dataSets(0)
+
+    assert(dataSet.variables.contains("var1"))
+    assert(dataSet.variables.contains("var2"))
+    assert(dataSet.variables.contains("var3"))
+    assert(dataSet.variables.size == 3)
+  }
+
   test("should add datasets for all files in directory") {
-    val directory = createTempDirectory
-    temporaryFile(directory, "test1.csv")
-    temporaryFile(directory, "test2.csv")
-    temporaryFile(directory, "test3.csv")
+    writeCorrectContent(temporaryFile("test1.csv"))
+    writeCorrectContent(temporaryFile("test2.csv"))
+    writeCorrectContent(temporaryFile("test3.csv"))
 
     DataSets.addFromPath(directory.toFile.getAbsolutePath)
     assert(DataSets.dataSets.size == 3)
-
-    deleteRecursively(directory)
   }
 
-  private def deleteRecursively(directory: Path): Unit = {
+  private def deleteRecursively(): Unit = {
     scalax.file.Path(directory.toFile).deleteRecursively()
   }
 
@@ -37,15 +102,20 @@ class DataSetsTest extends FunSuite {
     directory
   }
 
-  private def temporaryFile(directory: Path, filename: String): File = {
+  private def temporaryFile(filename: String): File = {
     val tempFile = new File(directory.toFile, filename)
     tempFile.createNewFile()
     tempFile
   }
 
-  private def temporaryFile: File = {
-    val file = File.createTempFile("temp", ".csv")
-    file.deleteOnExit()
-    file
+  private def writeCorrectContent(file: File): Unit = {
+    writeContent(file, Array(
+      "#var1,var2,var3",
+      "1,2,3"
+    ))
+  }
+
+  private def writeContent(file: File, content: Array[String]): Unit = {
+    Resource.fromFile(file).writeStrings(content, separator = "\n")
   }
 }
