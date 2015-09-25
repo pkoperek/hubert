0b3b21662f803e64346d66973ccb25c417862f15
Paweł Koperek <pkoperek@gmail.com>
2015-09-25 12:57:27 +0200

    Mutation and crossing over for fitness predictors

--- src/main/scala/pl/edu/agh/hubert/fitness/CoevolutionFitnessFunction.scala
+++ src/main/scala/pl/edu/agh/hubert/fitness/CoevolutionFitnessFunction.scala
@@ -2,16 +2,19 @@ package pl.edu.agh.hubert.fitness
 
 import pl.edu.agh.hubert.Individual
 import pl.edu.agh.hubert.datasets.CSVLoader
+import pl.edu.agh.hubert.engine.PopulationRepository
 import pl.edu.agh.hubert.experiments.Experiment
 import pl.edu.agh.hubert.hubert.Input
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
-class CoevolutionFitnessFunction(val experiment: Experiment) extends FitnessFunction {
+class CoevolutionFitnessFunction(val experiment: Experiment, val populationProvider: PopulationRepository) extends FitnessFunction {
+
+  type FitnessPredictor = Input
 
   private val loadedDataSet = CSVLoader.load(experiment.dataSet)
-  private var population = Array[Input]()
+  private var population = Array[FitnessPredictor]()
   private val fitnessPredictorSize = 4
   private val mutationProbability = 0.01
   private val crossOverProbability = 0.75
@@ -21,11 +24,11 @@ class CoevolutionFitnessFunction(val experiment: Experiment) extends FitnessFunc
     None
   }
 
-  private def generateIndividuals(howMuchToGenerate: Int): Array[Input] = {
-    (1 to howMuchToGenerate).map(_ => generateIndividual()).toArray
+  private def generateFitnessPredictors(howMuchToGenerate: Int): Array[Input] = {
+    (1 to howMuchToGenerate).map(_ => generateFitnessPredictor()).toArray
   }
 
-  private def generateIndividual(): Input = {
+  private def generateFitnessPredictor(): Input = {
     val predictorRows: Array[Int] = (1 to fitnessPredictorSize).map( _ => Random.nextInt(dataSetSize)).toArray
 
     val buffer = ArrayBuffer[Array[Double]]()
@@ -43,10 +46,45 @@ class CoevolutionFitnessFunction(val experiment: Experiment) extends FitnessFunc
   }
 
   private class MutationOperator {
+    def mutate(fitnessPredictor: FitnessPredictor): FitnessPredictor = {
+      if(Random.nextDouble() < mutationProbability) {
+        val pointToChange = Random.nextInt(fitnessPredictorSize)
+        val newRow = Random.nextInt(dataSetSize)
 
+        for(serieIdx <- fitnessPredictor.indices) {
+          fitnessPredictor(serieIdx)(pointToChange) = loadedDataSet.raw(serieIdx)(newRow)
+        }
+      }
+
+      fitnessPredictor
+    }
   }
 
   private class CrossOverOperator {
+
+    def crossOver(left: FitnessPredictor, right: FitnessPredictor): (FitnessPredictor, FitnessPredictor) = {
+      if(Random.nextDouble() < crossOverProbability) {
+        val crossOverPoint = Random.nextInt(fitnessPredictorSize)
+
+        val leftChild = ArrayBuffer[Array[Double]]()
+        val rightChild = ArrayBuffer[Array[Double]]()
+
+        for(serieIdx <- left.indices) {
+          val leftSerie = left(serieIdx)
+          val rightSerie = right(serieIdx)
+
+          val (leftChildHead, leftChildTail) = leftSerie.splitAt(crossOverPoint)
+          val (rightChildHead, rightChildTail) = leftSerie.splitAt(crossOverPoint)
+
+          leftChild += leftChildHead ++ rightChildTail
+          rightChild += rightChildHead ++ leftChildTail
+        }
+
+        return (leftChild.toArray, rightChild.toArray)
+      }
+
+      (left, right)
+    }
 
   }
 
