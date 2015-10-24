ef599d1fb349bf2ce8b60cf8ff7c0a1375cbf9b0
Paweł Koperek <pkoperek@gmail.com>
2015-10-24 16:03:44 +0100

    we should maximize the fitness function value

--- src/main/scala/pl/edu/agh/hubert/engine/EvolutionEngine.scala
+++ src/main/scala/pl/edu/agh/hubert/engine/EvolutionEngine.scala
@@ -31,7 +31,8 @@ private class DeterministicCrowdingEvolutionEngine(val experiment: Experiment) e
 
     population = tournament(groupedParents, evaluatedChildren)
 
-    val result = if (population.nonEmpty) population.minBy(_.fitnessValue) else "None!"
+    // assume that the higher fitness, the better
+    val result = if (population.nonEmpty) population.maxBy(_.fitnessValue) else "None!"
     logger.debug("Final fitness: " + result)
   }
 
