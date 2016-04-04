99a76f81c79abc5320215cc05538c0f8224bb8d2
Pawel Koperek <pkoperek@gmail.com>
2016-04-05 00:10:00 +0100

    fixed unit tests - this time for good

--- src/test/scala/pl/edu/agh/hubert/engine/DeterministicCrowdingEvolutionIterationTest.scala
+++ src/test/scala/pl/edu/agh/hubert/engine/DeterministicCrowdingEvolutionIterationTest.scala
@@ -6,10 +6,33 @@ import org.scalatest.FunSuite
 import pl.edu.agh.hubert.datasets.DataSet
 import pl.edu.agh.hubert.languages.math.DifferentiationWithFitnessPredictionFitnessFunction
 
+import scalax.io.Resource
+
 class DeterministicCrowdingEvolutionIterationTest extends FunSuite {
 
   test("should execute experiment with math language") {
-    val tmp = File.createTempFile("temp",".csv")
+    val tmp = File.createTempFile("temp", ".csv")
+    Resource.fromFile(tmp).writeStrings(List("#t,varB\n", "1.0,3.0\n", "2.0,4.0"))
+
+    val experiment = Experiment(
+      1,
+      "exp1",
+      "desc1",
+      10,
+      Languages.mathLanguage(),
+      new DataSet(tmp.getAbsolutePath, Set("t", "varB")),
+      fitnessFunction = classOf[DifferentiationWithFitnessPredictionFitnessFunction].getName
+    )
+
+    try {
+      new DeterministicCrowdingEvolutionIteration(experiment).evolve()
+    } catch {
+      case e: Throwable => fail("Exception thrown! ", e)
+    }
+  }
+
+  test("should throw exception when experiment doesn't contain time variable for math language") {
+    val tmp = File.createTempFile("temp", ".csv")
     val experiment = Experiment(
       1,
       "exp1",
@@ -20,11 +43,14 @@ class DeterministicCrowdingEvolutionIterationTest extends FunSuite {
       fitnessFunction = classOf[DifferentiationWithFitnessPredictionFitnessFunction].getName
     )
 
+    var exceptionThrown: Boolean = false
     try {
       new DeterministicCrowdingEvolutionIteration(experiment).evolve()
     } catch {
-      case e: Throwable => fail("Exception thrown! ", e)
+      case e: Throwable => exceptionThrown = true
     }
+
+    assert(exceptionThrown)
   }
 
 }
