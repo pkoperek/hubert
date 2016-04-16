d3f95444c490d06c9e44a184efc1bd6b3f6946e7
Pawel Koperek <pkoperek@gmail.com>
2016-04-16 17:36:22 +0100

    constant with the id of dx/dy variable

---
+++ src/main/scala/pl/edu/agh/hubert/languages/math/package.scala
@@ -0,0 +1,7 @@
+package pl.edu.agh.hubert.languages
+
+package object math {
+  // Special variable id denoting dx/dy. Data with this quotient
+  // is added as the last column in the input set
+  val Ddependent_Dvariable = -1
+}
