8b252b6a4685b289725b315271a80d7461419c23
Pawel Koperek <pkoperek@gmail.com>
2016-04-16 18:36:06 +0100

    refactor to independentVariable

--- src/main/scala/pl/edu/agh/hubert/languages/math/package.scala
+++ src/main/scala/pl/edu/agh/hubert/languages/math/package.scala
@@ -3,5 +3,5 @@ package pl.edu.agh.hubert.languages
 package object math {
   // Special variable id denoting dx/dy. Data with this quotient
   // is added as the last column in the input set
-  val Ddependent_Dvariable = -1
+  val independentVariable = -1
 }
